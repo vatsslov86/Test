4a38aeb984ea99d0f061ab3e72e7f74da28fd16f
Vasily <vatsslov86@mail.ru>
2017-05-14 11:29:05 +0400

    index

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="UTF-8">
+	<title>Кузовной ремонт покраска автомобиля в Пушкино</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Кузовные работы любой сложности в Пушкино. Ремонт кузова, шумоизоляция по низким ценам с использованием современного оборудования." />
+	<meta name="keywords" content="кузовной ремонт, кузовные работы, шумоизоляция, пушкино, автосервис в пушкино, вмятины, царапины, автомобиль, авто, покраска, полировка, бампер, восстановление, геометрия, кузов, машины, качественный, стоимость, цены, недорого" />
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<meta name="yandex-verification" content="73f2188fb6e7a7ae" />
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function (d, w, c) {
+			(w[c] = w[c] || []).push(function() {
+				try {
+					w.yaCounter44583928 = new Ya.Metrika({
+						id:44583928,
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true
+					});
+				} catch(e) { }
+			});
+
+			var n = d.getElementsByTagName("script")[0],
+				s = d.createElement("script"),
+				f = function () { n.parentNode.insertBefore(s, n); };
+			s.type = "text/javascript";
+			s.async = true;
+			s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+			if (w.opera == "[object Opera]") {
+				d.addEventListener("DOMContentLoaded", f, false);
+			} else { f(); }
+		})(document, window, "yandex_metrika_callbacks");
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/44583928" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+</head>
+<body>
+<header>
+	<div class="top_container">
+		<h1>Ремонт покраска кузова автомобиля</h1>
+		<br>
+		<strong>Качественно и недорого</strong>
+	</div>
+	<img src="images/header.jpg">
+</header>
+<section class="center">
+	<h2 align="center">Кузовной ремонт любой сложности в Пушкино</h2>
+	<div class="container">
+		<div class="breakpoint">
+			<img src="images/kuzov_remont.jpg" width="960" alt="Кузовной ремонт" />
+			<p>Кузовной ремонт</p>
+		</div>
+		<div class="breakpoint">
+			<img src="images/kuzov_paint.jpg" width="960" alt="Покраска авто" />
+			<p>Покраска авто</p>
+		</div>
+		<div class="breakpoint">
+			<img src="images/kuzov_vmyatina.jpg" width="960" alt="Удаление вмятин" />
+			<p>Удаление вмятин</p>
+		</div>
+		<div class="breakpoint">
+			<img src="images/zamena_stekla.jpg" width="960" alt="Вставка стёкол" />
+			<p>Вставка стёкол</p>
+		</div>
+		<div class="breakpoint">
+			<img src="images/kuzov_svarka.jpg" width="960" alt="Сварочные работы" />
+			<p>Сварочные работы</p>
+		</div>
+	</div>
+</section>
+<br>
+<br>
+<section>
+	<img src="images/brands.jpg">
+</section>
+<section id="contacts">
+	<div class="container">
+		<h2>Контакты</h2>
+		<h1>Записаться на сервис по телефону:<br> +7 977 261-22-01</h1>
+		<p>Пушкино ул.Учинская 6б</p>
+		<p>пн-вс с 09:00 до 22:00</p>
+	</div>
+	<div id="map">
+		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aba8933fbfd08fd9fde0be71c0973b7338f4a334afebbcb92a710de849b757c59&amp;width=100%25&amp;height=398&amp;lang=ru_RU&amp;scroll=true"></script>
+	</div>
+</section>
+</body>
+</html>
+<!-- Комент -->
